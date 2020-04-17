35608c87093a771973eb6d5cf3ec3ec513724931
James Jiang <monochrome473@gmail.com>
2020-04-17 19:47:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,22 +4,26 @@ require 'pry'
 def load_library(path)
   # code goes here
  emoji_hash = YAML.load_file('lib/emoticons.yml')
- emoji_hash
+=begin
  display_hash = {
    emoji_hash.keys[0] => {
      :english => emoji_hash.values[0][0],
      :japanese => emoji_hash.values[0][1]
    }
  }
+=end
+ display_hash = {}
  counter = 0
  while counter < emoji_hash.length do
    a_new_hash_element = {emoji_hash.keys[counter] => {
      :english => emoji_hash.values[counter][0],
      :japanese => emoji_hash.values[counter][1]
    }
+ }
    display_hash.merge!(a_new_hash_element)
    counter += 1
  end
+ display_hash
  binding.pry
  end
 
