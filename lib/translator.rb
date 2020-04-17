29688bff37d4f9f36bd40297b6fb3456b2cb3d80
James Jiang <monochrome473@gmail.com>
2020-04-17 19:34:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,12 @@ def load_library(path)
   # code goes here
  emoji_hash = YAML.load_file('lib/emoticons.yml')
  emoji_hash
- 
+ display_hash = {
+   emoji_hash.keys[0] => {
+     :english => emoji_hash.values[0][0]
+     :japanese => emoji_hash.values[0][1]
+   }
+ }
  binding.pry
  end
 
